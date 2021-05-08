040f4f5ae19a7c0cf0a7424d1f97e484a28b9618
pulgamecanica11 <pulgamecanica11@gmail.com>
2021-05-08 10:33:09 +0100

    More fixes

--- exercicio_4/carro.py
+++ exercicio_4/carro.py
@@ -8,49 +8,37 @@ def main():
 		nome = input("Escreve o nome: ")
 	print("Carros Disponiveis: ")
 	for model, values in cars.items():
-		print("-"*80)
-		print(f"({model}) - STATS: Capacidade do Deposito: {values['deposito']}L | Consumo: {values['consumo']}/100Km.")
+		print("-"*80, f"\n({model}) - STATS: Capacidade do Deposito: {values['deposito']}L | Consumo: {values['consumo']}/100Km.")
 		for value in values["art"]:
-			print(value)
-	car_option = int(input("Escolhe um dos carro: "))
-	if car_option == 1:
-		deposito = 100
-		capacidade = 300
-		consumo	= 50
-	elif car_option == 2:
-		deposito = 100
-		capacidade = 100 
-		consumo = 30
-	elif car_option == 3: 
-		deposito = 100
-		capacidade = 250 
-		consumo	= 40
-	else:
+			print("."*15, value)
+	car_option = int(input("Escolhe um dos carros "))
+	if car_option not in cars:
 		print("Opcao invalida!")
 		return -1
-	print("Escolheste a opcao", car_option)
-	car = Automovel(capacidade, deposito, consumo) if not answer else Automovel(capacidade, deposito, consumo, nome)
-	print(f"-----MENU ({car.nome}) Model{car_option}-----")
-	print(" (1) Consultar combustivel.\n", "(2) Autonomia.\n", "(3) Abastecer!\n","(4) Precorrer!\n", "(5) See car\n","(0) EXIT")
+	car = Automovel(cars[car_option]["deposito"], 50, cars[car_option]["consumo"]) if not answer else Automovel(cars[car_option]["deposito"], 50, cars[car_option]["consumo"], nome)
+	print(f"-----MENU ({car.nome}) Model{car_option}-----\n", "(1) Consultar combustivel.\n", "(2) Autonomia.\n", "(3) Abastecer!\n","(4) Precorrer!\n", "(5) Mostrar o Carro\n","(0) EXIT")
 	while True:	
-		option = int(input("Escolhe uma opcao: "))
-		if option == 1:
-			print("O combustivel disponivel é: ", car.combustivel(), "Litros.")
-		elif option == 2:
-			print("Autonomía: ", math.floor(car.autonomia()), "Km.")
-		elif option == 3:
-			car.abastece(int(input("Quantidade de combustivel(Litros): ")))
-		elif option == 4:
-			car.precorre(int(input("Distancia precorrida(Km): ")))
-		elif option == 5:
-			print("-"*80)
-			print(f"({car_option}) {car.nome} - STATS: Capacidade do Deposito: {cars[car_option]['deposito']}L | Consumo: {cars[car_option]['consumo']}/100Km.")
-			for value in cars[car_option]["art"]:
-				print(value)
-		elif option == 0:
-			print("Alright. Bye bye!")
-			break
-		else : 
+		option = input("Escolhe uma opcao: ")
+		try:
+			option = int(option)
+			if option == 1:
+				print("O combustivel disponivel é: ", car.combustivel(), "Litros.")
+			elif option == 2:
+				print("Autonomía: ", math.floor(car.autonomia()), "Km.")
+			elif option == 3:
+				car.abastece(int(input("Quantidade de combustivel(Litros): ")))
+			elif option == 4:
+				car.precorre(int(input("Distancia precorrida(Km): ")))
+			elif option == 5:
+				print("-"*80, f"\n({car_option}) {car.nome} - STATS: Capacidade do Deposito: {cars[car_option]['deposito']}L | Consumo: {cars[car_option]['consumo']}/100Km.")
+				for value in cars[car_option]["art"]:
+					print(value)
+			elif option == 0:
+				print("Alright. Bye bye!")
+				break
+			else : 
+				print("Opcao invalida!")
+		except :
 			print("Opcao invalida!")
 if __name__ == "__main__": 
   main()
