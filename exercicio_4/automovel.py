0a3033b58a61864f13badb1f94015d8553720100
pulgamecanica11 <pulgamecanica11@gmail.com>
2021-05-08 01:13:17 +0100

    Fixed menu

--- exercicio_4/automovel.py
+++ exercicio_4/automovel.py
@@ -10,10 +10,10 @@ class Automovel():
 	def autonomia(self):
 		return self.quant_comb * 100 / self.consumo
 	def abastece(self, n_litros):
-		self.quant_comb += n_litros
-		if self.quant_comb > self.cap_dep:
+		if self.quant_comb + n_litros > self.cap_dep:
 			print("Erro, nao pode abastecer mais do que o tanque permite...")
 			return -1
+		self.quant_comb += n_litros
 		print("Abasteceu...")
 	def precorre(self, n_km):
 		if n_km > self.autonomia():
